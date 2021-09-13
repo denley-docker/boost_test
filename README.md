257272b83574ad3554d9d8d93cc9810530f9f6eb
Denley Hsiao <denley@justtodo.com>
2021-09-14 00:22:48 +0800

    Add test files.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # boost_test
 Boost测试
+
+# 运行
+* `docker-compose up`
+* 最后一行出现`Hello, Boost <VERSION>`内容即为正常
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <boost/version.hpp>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+  std::cout << "Hello, Boost " << BOOST_VERSION << std::endl;
+  return 0;
+}
